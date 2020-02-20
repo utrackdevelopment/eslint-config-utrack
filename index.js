6cfe2cda814aa3caf8da6d9175a3788f64f9ee2a
Ben Clark <benclark@Bens-MacBook-Pro.local>
2020-02-20 14:30:31 +0000

    Add initial rules

---
+++ index.js
@@ -0,0 +1,88 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+    jest: true
+  },
+
+  parser: "babel-eslint",
+
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: "module",
+    ecmaFeatures: {
+      classes: true,
+      jsx: true
+    }
+  },
+
+  plugins: ["react", "redux-saga", "jsx-a11y"],
+
+  extends: ["eslint:recommended", "plugin:react/recommended", "airbnb-base"],
+
+  rules: {
+    "arrow-parens": ["error", "as-needed"],
+    "arrow-body-style": ["error", "as-needed"],
+    "comma-dangle": ["error", "always-multiline"],
+    "object-curly-spacing": "off",
+    "global-require": "off",
+    "consistent-return": "off",
+    "no-console": "warn",
+    "no-shadow": "warn",
+    indent: [
+      "error",
+      2,
+      {
+        SwitchCase: 1
+      }
+    ],
+    "prefer-template": "error",
+    "max-len": ["error", { code: 100 }],
+    "newline-per-chained-call": "off",
+    "no-confusing-arrow": "warn",
+    "no-plusplus": "off",
+    "quote-props": "off",
+    quotes: "off",
+    semi: 1,
+    "jsx-a11y/aria-props": "error",
+    "jsx-a11y/href-no-hash": "error",
+    "jsx-a11y/label-has-for": "error",
+    "jsx-a11y/mouse-events-have-key-events": "error",
+    "jsx-a11y/role-has-required-aria-props": "error",
+    "jsx-a11y/role-supports-aria-props": "error",
+    "redux-saga/no-yield-in-race": "error",
+    "redux-saga/yield-effects": "error",
+    "require-yield": "off",
+    "react/forbid-prop-types": "off",
+    "react/jsx-first-prop-new-line": ["error", "multiline"],
+    "react/jsx-filename-extension": "off",
+    "react/jsx-no-target-blank": "off",
+    "react/require-extension": "off",
+    "react/require-default-props": "off",
+    "import/no-webpack-loader-syntax": "off",
+    "import/newline-after-import": "off",
+    "import/no-dynamic-require": "off",
+    "import/no-extraneous-dependencies": "off",
+    "import/no-named-as-default": "off",
+    "import/no-unresolved": "error",
+    "import/prefer-default-export": "off"
+  },
+
+  settings: {
+    "import/resolver": {
+      webpack: {
+        config: "./config/webpack/webpack.production.babel.js"
+      }
+    }
+  },
+
+  globals: {
+    describe: true,
+    __DEV__: true,
+    it: true,
+    expect: true,
+    shallow: true,
+    beforeEach: true
+  }
+};
